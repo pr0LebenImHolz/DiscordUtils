84493311ca1b883b138ab4a366a3abf75b4b66e4
schwitze <66829261+litwdisc@users.noreply.github.com>
2022-05-14 20:26:12 +0200

    Implemented first draft of remove by UUID
    Needs Testing
    saveToFile method still missing

--- src/main/java/io/github/pr0lebenimholz/discordutils/linking/LinkDatabaseHandler.java
+++ src/main/java/io/github/pr0lebenimholz/discordutils/linking/LinkDatabaseHandler.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -19,6 +20,7 @@ import java.util.List;
  */
 public class LinkDatabaseHandler {
     private List<List<String>> registerCache = new ArrayList<>();
+    private final String FILEPATH = "";
     /*
      * ./world/@modid/PendingLinks.csv
      * ---
@@ -50,16 +52,60 @@ public class LinkDatabaseHandler {
     /*
      *  Adds a new Row to csv using provided data and set default rank to ftb
      */
-    public void addEntry(String uuid,String snowflake) throws IOException {
-        String rank = "ftb";
-        String[] user = {uuid, snowflake,rank};
-        //event call get start rank
-        writeToFile(user);
-        updateCache();
+    public void addEntry(String uuid,String snowflake) {
+       if(uuid != null && snowflake != null){
+           String rank = "ftb";
+           String[] user = {uuid, snowflake,rank};
+           ensureCache();
+           //event call get start rank
+           try {
+               writeToFile(user);
+           } catch (FileNotFoundException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+           updateCache();
+       }
+    }
+
+
+    public List<String> getLinkedByID(String checkID){
+        ensureCache();
+        for (List<String> player : registerCache){
+            for (String id : player){
+                if (id == checkID){
+                    return player;
+                }
+            }
+        }
+        return null;
     }
 
     public void removeEntry(String UUID){
         //todo
+        int index = getIndexByUUID(UUID);
+        if (index != -1){
+            registerCache.remove(index);
+            saveToFile();
+            updateCache();
+        }
+    }
+
+    /*
+     *  returns index in registerCache if not found returns -1
+     */
+    private int getIndexByUUID(String UUID){
+
+        int index = -1;
+        for (int i = 0; i<registerCache.size();i++){
+            List<String> buffer = registerCache.get(i);
+            if (buffer.contains(UUID)){
+                index = i;
+            }
+        }
+        return index;
     }
 
     /*
@@ -73,42 +119,75 @@ public class LinkDatabaseHandler {
     /*
      *  returns true if given id is contained
      */
-    public boolean isLinked(String id){
+    public boolean isLinked(String checkID){
         ensureCache();
-        if (registerCache.contains(id)){
+        for (List<String> player : registerCache){
+            for (String id : player){
+                if (id == checkID){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /*
+     *  ensures that cache is loaded and not empty unless csv is as well
+     */
+    private void ensureCache(){
+        try {
+            if (!isCSVempty()){
+              updateCache();
+            }else {
+                if (!registerCache.isEmpty()){
+                    updateCache();
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean isCSVempty() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(FILEPATH));
+        if (br.readLine() == null) {
             return true;
         }
         return false;
     }
 
     /*
-     *  ensures that cache is loaded and not empty
+     *  appends a new line to the csv
      */
-    private void ensureCache(){
-        //todo check if csv is empty and thus registerCache should be empty as well and no update is necessary
-        if (registerCache.isEmpty()) {
-            updateCache();
+    private void writeToFile(String[] user) throws IOException {
+        if (user != null){
+            if (!isLinked(user[0]) && !isLinked(user[1])) {
+                FileWriter fw = new FileWriter("FILEPATH", true);
+                fw.append(String.join(",",user));
+                fw.append("\n");
+                fw.flush();
+                fw.close();
+                updateCache();
+            }
         }
     }
 
     /*
-     *  appends a new line to the csv
+     *  saves the whole List registerCache into the given CSV
      */
-    private void writeToFile(String[] user) throws IOException {
-        //todo check duplicates
-        FileWriter fw = new FileWriter("PlayerReg", true);
-        fw.append(String.join(",",user));
-        fw.append("\n");
-        fw.flush();
-        fw.close();
-        updateCache();
+    private void saveToFile(){
+
     }
 
     /*
      *  reads the csv file and loads it into registerCache as List<List<String>> in form uuid | snowflake | rank
      */
     private void updateCache (){
-        try(BufferedReader br = new BufferedReader(new FileReader("PlayerReg"))) {
+        try(BufferedReader br = new BufferedReader(new FileReader("FILEPATH"))) {
         String line;
         List<List<String>> bufferCache = new ArrayList<>();
         while ((line=br.readLine()) != null){
